2fa9d88958478abe664fe076da1b6bb736baa5e5
Ryan Curry <16964784+ryancur@users.noreply.github.com>
2023-09-25 15:02:26 -0400

    moved models to a dict and code into a loop to train multiple models

--- data_science/src/model.py
+++ data_science/src/model.py
@@ -3,7 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from sklearn.model_selection import train_test_split
-#from sklearn.preprocessing import OneHotEncoder
+
+# from sklearn.preprocessing import OneHotEncoder
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import mean_squared_error
 from sklearn.linear_model import LinearRegression, SGDRegressor
@@ -15,18 +16,21 @@ from sklearn.svm import SVR
 import pickle
 import json
 
+
 def rmse(mse: float) -> float:
     """Calculate the Root Mean Squared Error"""
     return np.sqrt(mse)
 
+
 def read_from_json(file_handle: str) -> dict:
-    """ Load a json file """
+    """Load a json file"""
     with open(file_handle, "r") as open_file:
         json_object = json.load(open_file)
     return json_object
 
+
 def save_to_json(data: dict, file_handle: str) -> None:
-    """ Save python dictionary to a json file """
+    """Save python dictionary to a json file"""
     with open(file_handle, "w") as out_file:
         json.dump(data, out_file)
 
@@ -36,7 +40,7 @@ FEATURE_STORE_FILEPATH = "../models/model_feature_store.json"
 
 
 if __name__ == "__main__":
-    pd.set_option('display.max_columns', 50)
+    pd.set_option("display.max_columns", 50)
 
     # import data
     df = pd.read_csv("../data/clean_data/processed_data.csv")
@@ -72,7 +76,40 @@ if __name__ == "__main__":
     save_to_json(data=feature_store_dict, file_handle=FEATURE_STORE_FILEPATH)
 
     # do a train test split on the data
-    X_train, X_test, y_train, y_test = train_test_split(X_sparse_matrix, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X_sparse_matrix, y, test_size=0.2, random_state=42
+    )
+
+    models = {
+        "linear_regression": {
+            "model": LinearRegression(),
+            "train_rmse": None,
+            "test_rmse": None,
+        },
+        "random_forest": {
+            "model": RandomForestRegressor(),
+            "train_rmse": None,
+            "test_rmse": None,
+        },
+        "svm": {"model": SVR(), "train_rmse": None, "test_rmse": None},
+    }
+
+    for model_type, model_data in models.items():
+        model = model_data["model"]
+        print(f"Creating {model_type} model...")
+        model.fit(X_train, y_train)
+        preds = model.predict(X_train)
+        mse_train = mean_squared_error(y_train, preds)
+        rmse_train = rmse(mse_train)
+        print(f"Training Set {model_type} RMSE: ", rmse_train)
+        pred_test = model.predict(X_test)
+        mse_test = mean_squared_error(y_test, pred_test)
+        rmse_test = rmse(mse_test)
+        print(f"Test Set {model_type} RMSE: ", rmse_test)
+        models[model_type]["train_rmse"] = rmse_train
+        models[model_type]["test_rmse"] = rmse_test
+
+    # compare the test rmse for each model and select the model with the lowest value
 
     # instantiate the model
     # model_type = "Linear Regression"
@@ -81,45 +118,45 @@ if __name__ == "__main__":
     # model_type = "Gaussian Naive Bayes"
     # model = GaussianNB()
 
-    model_type = "Random Forest"
-    model = RandomForestRegressor()
+    # model_type = "Random Forest"
+    # model = RandomForestRegressor()
 
-    # model_type = "Support Vector Machine"
-    # model = SVR()
+    # # model_type = "Support Vector Machine"
+    # # model = SVR()
 
-    print(f"Creating {model_type} model...")
+    # print(f"Creating {model_type} model...")
 
-    # train model - fit
-    model.fit(X_train, y_train)
+    # # train model - fit
+    # model.fit(X_train, y_train)
 
-    # use the model to predict on the training set
-    preds = model.predict(X_train)
+    # # use the model to predict on the training set
+    # preds = model.predict(X_train)
 
-    # evaluate the model on the training set using MSE and RMSE
-    mse_train = mean_squared_error(y_train, preds)
-    rmse_train = rmse(mse_train)
-    print(f"Training Set {model_type} RMSE: ", rmse_train)
+    # # evaluate the model on the training set using MSE and RMSE
+    # mse_train = mean_squared_error(y_train, preds)
+    # rmse_train = rmse(mse_train)
+    # print(f"Training Set {model_type} RMSE: ", rmse_train)
 
-    # evaluate model on the test set
-    pred_test = model.predict(X_test)
-    mse_test = mean_squared_error(y_test, pred_test)
-    rmse_test = rmse(mse_test)
-    print(f"Test Set {model_type} RMSE: ", rmse_test)
+    # # evaluate model on the test set
+    # pred_test = model.predict(X_test)
+    # mse_test = mean_squared_error(y_test, pred_test)
+    # rmse_test = rmse(mse_test)
+    # print(f"Test Set {model_type} RMSE: ", rmse_test)
 
     ### PROD MODEL TRAINING ###
 
     # re-train model for production with all the data
-    prod_model = RandomForestRegressor()
-    prod_model.fit(X_sparse_matrix, y)
-    preds_prod = prod_model.predict(X_sparse_matrix)
-    mse_prod = mean_squared_error(y, preds_prod)
-    rmse_prod = rmse(mse_prod)
-    print(f"Production Model {model_type} RMSE: ", rmse_prod)
+    # prod_model = RandomForestRegressor()
+    # prod_model.fit(X_sparse_matrix, y)
+    # preds_prod = prod_model.predict(X_sparse_matrix)
+    # mse_prod = mean_squared_error(y, preds_prod)
+    # rmse_prod = rmse(mse_prod)
+    # print(f"Production Model {model_type} RMSE: ", rmse_prod)
 
-    ### SAVE TRAINED PROD MODEL ###
+    # ### SAVE TRAINED PROD MODEL ###
 
-    # pickle model
-    print("Pickling the model...")
-    pickle.dump(prod_model, open(MODEL_FILEPATH, "wb"))
+    # # pickle model
+    # print("Pickling the model...")
+    # pickle.dump(prod_model, open(MODEL_FILEPATH, "wb"))
 
-    print("Done.")
+    # print("Done.")
